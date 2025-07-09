7c35751940f9180ea4ab94b450036c5c872da876
beansprobeans <matthy13@uci.edu>
2025-07-09 14:29:19 -0700

    test sprites
    le epic

---
+++ Assets/scripts/script.cs
@@ -0,0 +1,41 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class script : MonoBehaviour
+//{
+//    [SerializeField] private float speed;
+//    private Rigidbody2D body;
+
+//    //private float speed = 8f;
+//    //private float jumpingPower = 16f;
+    
+
+//    //[SerializeField] private Rigidbody2D rb;
+//    //[SerializeField] private Transform groundCheck;
+//    //[SerializeField] private LayerMask groundLayer;
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+        
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+
+        
+
+//        if (Input.GetKey(KeyCode.D))
+//        {
+//            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+//        }
+
+//        if (Input.GetKey)
+//    }
+
+//    private void FixedUpdate()
+//    {
+//        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+//    }
+//}
